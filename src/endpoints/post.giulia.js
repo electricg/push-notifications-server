f0fb1c5048e62027ef95967cc7639edbb9b4992f
Giulia Alfonsi <electric.g@gmail.com>
2018-07-20 11:36:11 -0700

    Stringify the object

--- src/endpoints/post.giulia.js
+++ src/endpoints/post.giulia.js
@@ -1,7 +1,10 @@
 module.exports.handler = function(request, reply) {
   const { payload, headers } = request;
 
-  console.log({ payload, headers });
+  console.log({
+    payload: JSON.stringify(payload),
+    headers: JSON.stringify(headers),
+  });
 
   reply({ status: 1, payload, headers });
 };
